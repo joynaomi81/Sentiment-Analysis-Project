b45543ee6896185c187b13d9efec470f77ebee87
Olusanya Joy Naomi <91166707+joynaomi81@users.noreply.github.com>
2024-08-07 14:37:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 
 #CountVectorizer and model from pickle files
 cv = pickle.load(open('cv-vectorizer.pkl', 'rb'))
-model = pickle.load(open('Movies_Review_Sentiment_Analysis.pkl', 'rb'))
+model = pickle.load(open('Movie_Reviews_Sentiment_Analysis.pkl', 'rb'))
 
 def predict_sentiment(text):
     """Predict the sentiment of the input text."""
@@ -13,7 +13,7 @@ def predict_sentiment(text):
 
 # Streamlit app
 def main():
-    st.title("Sentiment Analysis App")
+    st.title("Movie Reviews Sentiment Analysis App")
     st.write("I hate the movie:")
 
     # User input
@@ -24,7 +24,7 @@ def main():
             sentiment = predict_sentiment(user_input)
             st.write(f"The predicted sentiment is: {'Positive' if sentiment == 1 else 'Negative'}")
         else:
-            st.write("Please enter some text to analyze.")
+            st.write('I love the movie.')
 
 if __name__ == "__main__":
     main()
