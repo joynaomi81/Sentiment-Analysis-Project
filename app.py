33e86328735b15ee84d5943fcbd657f6fd076ea9
Olusanya Joy Naomi <91166707+joynaomi81@users.noreply.github.com>
2024-08-07 15:23:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 # Download necessary NLTK data
 nltk.download('stopwords')
