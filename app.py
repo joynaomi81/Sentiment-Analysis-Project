b55e2e0ee968c007e3e3338660c84faa661a00f1
Olusanya Joy Naomi <91166707+joynaomi81@users.noreply.github.com>
2024-10-31 08:43:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,22 @@ import streamlit as st
 import joblib
 
 # Load the trained model
-model = joblib.load('model.pkj')  # Ensure this path is correct
-
+model = joblib.load('model.pkl')  
 # Title of the app
 st.title("Sentiment Analysis App")
 
 # Text input from the user
-user_input = st.text_input("Enter text here:")
+user_input = st.text_input("Enter a text here:")
 
 # Predict button
 if st.button("Predict"):
     if user_input:
         # Make prediction
         prediction = model.predict([user_input])
-
-        # Display result with emoji
-        if prediction[0] == 1:
-            sentiment = "Positive 😊"
-        else:
-            sentiment = "Negative 😞"
         
+        # print the result
+        sentiment = "Positive" if prediction[0] == 1 else "Negative"
         st.write(f'Sentiment: {sentiment}')
     else:
         st.write("Please enter text to analyze.")
+
